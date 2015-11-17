be0397991aa18dbb7ab79d067f9dd4bdc9795c55
Ronald Chaplin <rchaplin@t73.biz>
2015-11-16 22:08:08 -0600

    Implementing logic

--- js/calculator.js
+++ js/calculator.js
@@ -1,9 +1,69 @@
 var calculator = angular.module('calculator', []);
 
 calculator.controller('CalculatorController', function ($scope) {
-	$scope.answer = 0;
+	var digits = [];
+	$scope.display = 0;
+	$scope.equation = {
+		operands: [],
+		operator: ''
+	};
 
-	$scope.addOperand = function(operand) {
-		$scope.answer += operand.toString();
+	var renderDisplay = function(number) {
+		if(number.length < 11) {
+			$scope.display = number;
+		} else {
+			$scope.display = parseFloat(number).toExponential(6);
+		}
 	}
+
+	$scope.clearDisplay = function() {
+		$scope.display = 0;
+	};
+
+	$scope.clearEverything = function() {
+		$scope.clearDisplay();
+		$scope.equation.operands = [];
+		$scope.equation.operator = '';
+		digits = [];
+	};
+
+	$scope.addDigit = function(number) {
+		digits.push(number.toString());
+		display = digits.join('').toString();
+
+		renderDisplay(display);
+	};
+
+	$scope.setDecimal = function() {
+		if(digits.length < 11 && digits.indexOf('.') === -1) {
+			digits.push('.');
+		}
+	};
+
+
+	$scope.addOperator = function(operator) {
+		$scope.equation.operator= operator;
+		if($scope.equation.operands.length == 2){
+			$scope.equation.operands.pop();
+		} else {
+			$scope.equation.operands.push(digits.join('').toString());
+			digits = [];
+			renderDisplay('0');
+		}
+	}
+
+	$scope.evalEquation = function() {
+		var equate = '';
+		if ($scope.equation.operands.length == 1) {
+			$scope.equation.operands.push(digits.join(''));
+			digits = [];
+		}
+		equate = $scope.equation.operands.join($scope.equation.operator);
+		var answer = eval(equate);
+		$scope.equation.operands.shift();
+		$scope.equation.operands.unshift(answer.toString());
+
+		renderDisplay(answer.toString());
+	}
+
 });
